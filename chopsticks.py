14358a9e29736a99c50e7e288230ee33a5587df5
Suyash458 <sne9x@outlook.com>
2016-09-03 18:21:13 +0530

    Major rework. Changed objective function

--- chopsticks.py
+++ chopsticks.py
@@ -1,20 +1,10 @@
 import numpy as np
 import random
 
-indexHash = {}
-partitionsHash = {}
 
-
-class State:
-    def __init__(p1, p2):
-        self.state = (p1, p2)
-        self.transitions = self.get_possible_transitions(p1, p2, True)
-        self.indexHash = {x:y for x, y in zip(self.transitions, range(len(self.transitions)))}
-        self.reward_matrix = np.zeros(1, len(self.transitions))
-        self.reward_matrix[:] = np.nan
-
-    def __str__(self):
-        return self.state
+from_states = {}
+to_states = {}
+state_matrix = None
 
 def two_partitions(n):
     '''
@@ -27,22 +17,13 @@ def two_partitions(n):
         a, b = n-4, 4
     return zip(range(a, b+1), range(b, a-1, -1))
 
-def get_indices():
-    '''
-    Get matrix indices hashed to partitions
-    '''
-    for i in xrange(9):
-        partitions = two_partitions(i)
-        partitionsHash[i] = partitions
-
 def get_matrix():
     '''
     Compute all possible from states and to states 
     Construct a matrix of 576 x 624 (all NaN)
     Make a hash table of states to matrix indices
     '''
-    from_states = {}
-    to_states = {}
+    global state_matrix
     from_index = 0
     to_index = 0
     for i in xrange(9):
@@ -61,7 +42,7 @@ def get_matrix():
                         from_index += 1
     state_matrix = np.zeros((len(from_states), len(to_states)))
     state_matrix[:] = np.nan
-    return from_states, to_states, state_matrix
+    #return from_states, to_states, state_matrix
 
 def get_possible_transitions(p1, p2, turn):
     '''
@@ -86,14 +67,13 @@ def get_possible_transitions(p1, p2, turn):
     if b and c:
         transitions.append([p1, (b+c, d)]) if b+c < 5 else transitions.append([p1, (0, d)])
     if p1.__contains__(0):
-        if sum(p1) % 2 == 0:
-            for partition in partitionsHash[sum(p1)]:
-                if not partition.__contains__(0):
-                    transitions.append([partition, p2])
-    #else:
-    #    for partition in partitionsHash[sum(p1)]:
-    #        if not partition.__contains__(0) and partition != p1:
-    #            transitions.append([partition, p2])
+        for partition in two_partitions(sum(p1)):
+            if not partition.__contains__(0):
+                transitions.append([partition, p2])
+    else:
+        for partition in two_partitions(sum(p1)):
+            if not partition.__contains__(0) and partition != p1:
+                transitions.append([partition, p2])
         #transitions.append([(a+b, b), p2]) if a+b < 5 else transitions.append([(0, b), p2])
         #transitions.append([(a, a+b), p2]) if a+b < 5 else transitions.append([(a, 0), p2])
     if not turn:
@@ -103,13 +83,13 @@ def get_possible_transitions(p1, p2, turn):
     return list(set(transitions))
 
 
-def play_game_better(from_states, to_states, state_matrix, turn=True):
+def play_game_better(turn=True):
     game = []
     p1 = (1, 1)
     p2 = (1, 1)
     game.append((p1, p2))
     while True:
-        transition = get_best_move((p1, p2), from_states, to_states, state_matrix, turn)
+        transition = get_best_move((p1, p2), turn)
         if transition:
             game.append(transition)
         else:
@@ -147,82 +127,75 @@ def play_game(turn=True):
             break
     return game
 
-def condition_matrix(from_states, to_states, state_matrix):
+def condition_matrix():
     for state in from_states.keys():
         for transition in get_possible_transitions(state[0], state[1], True):
             if not any(transition[1]):
-                state_matrix[from_states[state], to_states[transition]] = 1000000
-            if not any(transition[0]):
-                state_matrix[from_states[state], to_states[transition]] = -1000000
-        for transition in get_possible_transitions(state[0], state[1], False):
-            if not any(transition[1]):
-                state_matrix[from_states[state], to_states[transition]] = -1000000
-            if not any(transition[0]):
-                state_matrix[from_states[state], to_states[transition]] = 1000000                  
+                state_matrix[from_states[state], to_states[transition]] = 1000             
     return state_matrix
 
-def get_best_move(state, from_states, to_states, state_matrix, turn=True):
+def get_best_move(state, turn=True):
     '''
     Find out possible transitions
     Get the maximum reward for those
     return the best transition
     '''
+    global state_matrix
     if not turn:
         state = (state[1], state[0])
-    transitions = get_possible_transitions(state[0], state[1], True)
-    indices = [to_states[i] for i in transitions]
     try:
-        best_index = np.nanargmax(state_matrix[from_states[state]][indices])
-    except:
+        best_index = np.nanargmax(state_matrix[from_states[state]])
+    except Exception as e:
+        print e
         return None
     best_trans = None
-    for trans in transitions:
-        if to_states[trans] == indices[best_index]:
+    for trans in to_states:
+        if to_states[trans] == best_index:
             best_trans = trans
     if not turn:
         best_trans = (best_trans[1], best_trans[0])
     return best_trans
 
+def update_state(current_state, next_state, reward, learning_rate, discount, swap=False):
+    global state_matrix
+    if swap:
+        current_state = (current_state[1], current_state[0])
+        next_state = (next_state[1], next_state[0])
+    opponent_state = (next_state[1], next_state[0])
+    prevQ = state_matrix[from_states[current_state]][to_states[next_state]]
+    if np.isnan(prevQ):
+        prevQ = 0
+    try:
+        max_opponent_score = np.nanmax(state_matrix[from_states[opponent_state]])
+        if np.isnan(max_opponent_score):
+            max_opponent_score = 0
+    except KeyError:
+        max_opponent_score = -1000
+    currentQ = (1 - learning_rate)*prevQ + learning_rate*discount*(reward - max_opponent_score)
+    #print "Updating ",
+    #print from_states[current_state], to_states[next_state], current_state, next_state, opponent_state, prevQ, currentQ
+    state_matrix[from_states[current_state]][to_states[next_state]] = currentQ
+
+
 def train_better(num_tries=10000):
-    from_states, to_states, state_matrix = train(10000)
-    get_indices() 
-    win=1 
-    reward = 50000
-    discount = 0.8
+    train(10000)
+    get_matrix()
+    condition_matrix()
+    discount = 0.7
     init_learning_rate = 0.9
     learning_decay = 1 
-    learning_decay_step = 0.6/(num_tries)
+    learning_decay_step = 1/(num_tries)
     for i in xrange(num_tries):
-        game = play_game_better(from_states, to_states, state_matrix, True)
+        game = play_game_better()
         learning_rate = init_learning_rate/learning_decay  
         learning_decay += learning_decay_step
-        if len(game) % 2 == 0:
-            win = 1
-        else:
-            win = -1
+        turn = len(game)%2 == 0
+        reward = 100 if turn else -100
         for index in range(0, len(game) - 1):
-            row, col = from_states[game[index]], to_states[game[index+1]]
-            nextQ = np.nan
-            if index != len(game) - 2:
-                if win == 1:
-                    nextQ = np.nanmax(state_matrix[from_states[game[index+1]]])
-                else:
-                    nextQ = -1 * np.nanmax(state_matrix[from_states[game[index+1]]]) 
-            else:
-                if win == 1:
-                    nextQ = 1000000
-                else:
-                    nextQ = -1000000               
-            update_value = win*learning_rate*reward
-            if not np.isnan(nextQ):
-                update_value += update_value + discount*learning_rate*nextQ
-            if not np.isnan(state_matrix[row, col]):
-                update_value += (1 - learning_rate) * state_matrix[row, col]
-            #print game[index], game[index+1], win, learning_rate, discount, nextQ,  reward, state_matrix[row][col], update_value
-            state_matrix[row][col] = update_value
-            win *= -1
-        #print " "
-    return from_states, to_states, state_matrix 
+            update_state(game[index], game[index+1], reward, learning_rate, discount, turn)
+            turn = not turn
+            reward = -reward
+    #return from_states, to_states, state_matrix 
 
 def train(num_tries=10000):
     '''
@@ -235,44 +208,23 @@ def train(num_tries=10000):
         do the opposite
     decrease learning rate and increase discount with time
     '''
-    get_indices()
-    win=1
-    from_states, to_states, state_matrix = get_matrix()
-    reward = 5000
-    discount = 0.8
+    get_matrix()
+    condition_matrix()
+    discount = 0.7
     init_learning_rate = 0.9
-    state_matrix = condition_matrix(from_states, to_states, state_matrix)
     learning_decay = 1 
     learning_decay_step = 1/(num_tries)
-    game_func = play_game
     for i in xrange(num_tries):
         game = play_game()
         learning_rate = init_learning_rate/learning_decay  
         learning_decay += learning_decay_step
-        if len(game) % 2 == 0:
-            win = 1
-        else:
-            win = -1
-        for index in range(0, len(game) - 1):
-            row, col = from_states[game[index]], to_states[game[index+1]]
-            nextQ = np.nan
-            if index != len(game) - 2:
-                if win == 1:
-                    nextQ = np.nanmax(state_matrix[from_states[game[index+1]]])
-                else:
-                    nextQ = -1 * np.nanmax(state_matrix[from_states[game[index+1]]]) 
-            else:
-                if win == 1:
-                    nextQ = 1000000
-                else:
-                    nextQ = -1000000                 
-            update_value = win*learning_rate*reward/(len(game) - index)
-            if not np.isnan(nextQ):
-                update_value += update_value + discount*learning_rate*nextQ
-            if not np.isnan(state_matrix[row, col]):
-                update_value += (1 - learning_rate) * state_matrix[row, col]
-            #print game[index], game[index+1], win, learning_rate, discount, nextQ,  reward, state_matrix[row][col], update_value
-            state_matrix[row][col] = update_value
-            win *= -1
+        win = len(game) % 2 == 0
+        reward = 50 if win else -50
+        swap = False
+        for index in range(len(game) - 1):
+            #print game[index], game[index+1], swap, reward
+            update_state(game[index], game[index+1], reward, learning_rate, discount, swap)
+            swap = not swap
+            reward = -reward
         #print " "
-    return from_states, to_states, state_matrix
+    #return from_states, to_states, state_matrix
